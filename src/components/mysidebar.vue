80b97135b8ed9cc8973edbadea8145b0a16decd7
xujunlong <874435168@qq.com>
2018-06-02 14:23:45 +0800

    列表展示完成

---
+++ src/components/mysidebar.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class="col-sm-3 col-md-2 sidebar">
+      <ul class="nav nav-sidebar">
+        <router-link tag="li" to="/mysidebar/list" active-class="active"><a href="#">英雄列表</a></router-link>
+        <router-link tag="li" to="/equip/list" active-class="active"><a href="#">武器列表</a></router-link>
+        <router-link tag="li" to="/weapon/list" active-class="active"><a href="#">装备列表</a></router-link>
+      </ul>
+ </div>
+</template>
+
+<script>
+import MySidebarList from '@/views/heroes/list'
+import EquipList from '@/views/equip/list'
+import WeaponList from '@/views/weapon/list'
+export default {
+  name: 'mysidebar',
+  components: {
+    MySidebarList,
+    EquipList,
+    WeaponList
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import MySidebarList from '@/views/heroes/list'
+import EquipList from '@/views/equip/list'
+import WeaponList from '@/views/weapon/list'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    { name: 'home', path: '/', redirect: { name: 'mysidebarList' } },
+    { name: 'mysidebarList', path: '/mysidebar/list', component: MySidebarList },
+    { name: 'equiplist', path: '/equip/list', component: EquipList },
+    { name: 'weaponlist', path: '/weapon/list', component: WeaponList }
   ]
 })
